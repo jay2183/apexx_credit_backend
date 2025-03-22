abc08d01cae6a9c97d09e7f9f959dd1da6fd37d1
Jay <jaybhardwaj17601@gmail.com>
2025-03-22 11:14:06 +0530

    bill num

--- app/models/billNum.js
+++ app/models/billNum.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
 const billNumSchema = new mongoose.Schema({
-    type: Number,
-    required: true,
-    default: 1,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+        unique: true, // Ensures each user has only one BillNum entry
+      },
+      billNumber: {
+        type: Number,
+        required: true,
+        default: 1, // Start from 1
+      },
 })
 
 module.exports = mongoose.model('BillNum', billNumSchema);
